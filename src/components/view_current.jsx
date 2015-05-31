9c6ae445377100d1f8f126c423c4dbfbb1370f4d
dgellow <samuel.elborai@gmail.com>
2015-05-31 17:15:28 +0200

    dynamically render ViewCurrent with dummy initial data

--- src/components/view_current.jsx
+++ src/components/view_current.jsx
@@ -5,35 +5,51 @@
 var React = require('react');
 
 class ViewCurrent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            songs: [
+                {
+                    cover: '',
+                    title: 'Money',
+                    album: 'The Dark Side of the Moon',
+                    artist: 'Pink Floyd'
+                },
+                {
+                    cover: '',
+                    title: 'Time',
+                    album: 'The Dark Side of the Moon',
+                    artist: 'Pink Floyd'
+                },
+                {
+                    cover: '',
+                    title: 'On the Run',
+                    album: 'The Dark Side of the Moon',
+                    artist: 'Pink Floyd'
+                }
+            ]
+        };
+    }
+
+    renderSongs(songs) {
+        return [].map.call(songs, (song) => {
+            return (
+                <li className="song row">
+                    <div className="cover one column">{song.cover}</div>
+                    <div className="title four columns">{song.title}</div>
+                    <div className="six columns">
+                        <div className="album">{song.album}</div>
+                        <div className="artist">{song.artist}</div>
+                    </div>
+                </li>
+            );
+        });
+    }
+
     render() {
         return (
             <ul id="view-current-songs">
-                <li className="song row">
-                    <div className="cover one column">CVR</div>
-                    <div className="title four columns">Money</div>
-                    <div className="six columns">
-                        <div className="album">The Dark Side of the Moon</div>
-                        <div className="artist">Pink Floyd</div>
-                    </div>
-                </li>
-
-                <li className="song row">
-                    <div className="cover one column">CVR</div>
-                    <div className="title four columns">Money</div>
-                    <div className="six columns">
-                        <div className="album">The Dark Side of the Moon</div>
-                        <div className="artist">Pink Floyd</div>
-                    </div>
-                </li>
-
-                <li className="song row">
-                    <div className="cover one column">CVR</div>
-                    <div className="title four columns">Money</div>
-                    <div className="six columns">
-                        <div className="album">The Dark Side of the Moon</div>
-                        <div className="artist">Pink Floyd</div>
-                    </div>
-                </li>
+                {this.renderSongs(this.state.songs)}
             </ul>
         );
     }
