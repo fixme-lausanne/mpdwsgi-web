079c15b51a2e0bc6d6ca40edeb44269f2aece39f
dgellow <samuel.elborai@gmail.com>
2015-06-05 10:34:47 +0200

    reactive ui for play/pause button

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -9,18 +9,49 @@ import Actions from './Actions.jsx';
 import ProgressBar from './ProgressBar.jsx';
 
 export default class Player extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // I use a state 'isPlaying' local to this component for
+            // reactive UI.
+            // http://en.wikipedia.org/wiki/Reactive_user_interface
+            // The idea is to update the UI, then call the api and
+            // change back to previous state if it didn't work.
+            isPlaying: false
+        };
+    }
+
+    optimisticUpdate(newState, fn) {
+        this.setState({isPlaying: newState === 'play'});
+        if (fn) {
+            fn.call();
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({isPlaying: nextProps.isPlaying});
+    }
+
+    componentDidMount() {
+        this.setState({isPlaying: this.props.isPlaying});
+    }
+
     render() {
         let playerContent = this.props.song ?
                 [React.createElement(Info, {song: this.props.song}),
                  React.createElement(Actions, {
-                     isPlaying: this.props.isPlaying,
+                     isPlaying: this.state.isPlaying,
                      onClickPrevious: actions.previous,
                      onClickNext: actions.next,
-                     onClickPlay: actions.play,
-                     onClickPause: actions.pause
+                     onClickPlay: this.optimisticUpdate.bind(
+                         this, 'play', actions.play
+                     ),
+                     onClickPause: this.optimisticUpdate.bind(
+                         this, 'pause', actions.pause
+                     )
                  }),
                  React.createElement(ProgressBar, {
-                     isPlaying: this.props.isPlaying,
+                     isPlaying: this.state.isPlaying,
                      song: this.props.song,
                      onClick: actions.seek
                  })]: null;
